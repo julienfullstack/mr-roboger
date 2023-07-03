f9efd03e5d14bf1f7729614e7311b9cec31872d9
Julien L <julienlenaz96@gmail.com>
2023-07-03 18:09:14 -0400

    Returned to else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -16,17 +16,19 @@ function beepBoop(userInput) {
   } else {
     const inputArray = Array.from(userInput.toString());
     const finalOutput = [];
+
     for (let i = 0; i < inputArray.length; i++) {
-      unless (!inputArray[i].includes('3')) {
+      if (inputArray[i].includes('3')) {
         finalOutput.push("Won't you be my neighbor?");
-      } unless (!inputArray[i].includes('2')) {
-        finalOutput.push("Boop");
-      } unless (!inputArray[i].includes('1')) {
-        finalOutput.push("Beep");
+      } else if (inputArray[i].includes('2')) {
+        finalOutput.push("Boop!");
+      } else if (inputArray[i].includes('1')) {
+        finalOutput.push("Beep!");
       } else {
         finalOutput.push(inputArray[i]);
       }
     }
+
     return finalOutput.join("");
-    }
-   }
+  }
+}
