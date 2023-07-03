0bf77c90938bb48bc91335b169c9850a5cc8c5b3
Julien L <julienlenaz96@gmail.com>
2023-07-03 16:49:24 -0400

    Debugged window event listener and handleformsubmission function

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,20 @@
 /// BUSINESS LOGIC ///
-window.addEventListener("load", function() {
-  let form = document.getElementById("form");
-  let submit = document.querySelector("#form button[type='submit']");
-  submit.addEventListener("click", handleFormSubmission);
-});
+window.addEventListener("load", function() { let form = document.getElementById("form"); let submit = document.querySelector("#form button[type='submit']"); submit.addEventListener("click", handleFormSubmission); });
 
 function handleFormSubmission(event) {
   event.preventDefault();
   let userInput = document.getElementById("input").value;
   let outputArray = beepBoop(userInput);
-  document.getElementById("output").innerText = outputArray;
+  document.getElementById("output").innerText = outputArray.join(", ");
 }
 
 function beepBoop(userInput) {
-  if (typeof userInput !== "number") {
+  if (isNaN(userInput)) {
     return "Sorry, your input is not a number!";
   } else {
     const inputArray = Array.from(userInput.toString());
     inputArray.unshift("0");
-    const outputArray = inputArray.map((digit) => digit === "1" ? "Beep" : digit);
+    const outputArray = inputArray.map((digit) => (digit === "1" ? "Beep" : digit));
     return outputArray;
   }
 }
