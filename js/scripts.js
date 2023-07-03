ade2df2731748980362e6e4752b5b9b48cccd945
Julien L <julienlenaz96@gmail.com>
2023-07-03 16:42:03 -0400

    Debugged script

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
+/// BUSINESS LOGIC ///
 window.addEventListener("load", function() {
   let form = document.getElementById("form");
   let submit = document.querySelector("#form button[type='submit']");
-
   submit.addEventListener("click", handleFormSubmission);
 });
 
 function handleFormSubmission(event) {
   event.preventDefault();
   let userInput = document.getElementById("input").value;
-  document.getElementById("output").innerText = beepBoop(input);
+  let outputArray = beepBoop(userInput);
+  document.getElementById("output").innerText = outputArray;
 }
-
 
 function beepBoop(userInput) {
   if (typeof userInput !== "number") {
     return "Sorry, your input is not a number!";
+  } else {
+    const inputArray = Array.from(userInput.toString());
+    inputArray.unshift("0");
+    const outputArray = inputArray.map((digit) => digit === "1" ? "Beep" : digit);
+    return outputArray;
   }
-  
-  const inputArray = Array.from(userInput.toString());
-  inputArray.unshift("0");
-
-  return inputArray;
 }
